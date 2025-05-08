cce62d8c0ed0952f1f336ad4829e721bb1ee6448
anjal <anjalpoudel@gmail.com>
2025-05-08 13:37:37 +0530

    Add the PVReclaim CR  structure in [api/v1alpha1]

--- api/v1alpha1/pvreclaim_types.go
+++ api/v1alpha1/pvreclaim_types.go
@@ -1,46 +1,34 @@
-/*
-Copyright 2025.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // PVReclaimSpec defines the desired state of PVReclaim.
 type PVReclaimSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of PVReclaim. Edit pvreclaim_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:MinLength=1
+	PersistentVolumeName string `json:"persistentVolumeName"`
 }
 
 // PVReclaimStatus defines the observed state of PVReclaim.
+type PVReclaimPhase string
+
+// for listing all the possible valid values
+const (
+	PendingPhase PVReclaimPhase = "Pending"
+	SuccessPhase PVReclaimPhase = "Success"
+	FailurePhase PVReclaimPhase = "Failed"
+)
+
 type PVReclaimStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	Phase              PVReclaimPhase `json:"phase,omitempty"`
+	ReclaimedTimestamp *metav1.Time   `json:"reclaimedTimestamp,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
-
 // PVReclaim is the Schema for the pvreclaims API.
 type PVReclaim struct {
 	metav1.TypeMeta   `json:",inline"`
@@ -51,7 +39,6 @@ type PVReclaim struct {
 }
 
 // +kubebuilder:object:root=true
-
 // PVReclaimList contains a list of PVReclaim.
 type PVReclaimList struct {
 	metav1.TypeMeta `json:",inline"`
